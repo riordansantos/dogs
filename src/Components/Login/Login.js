62b856f04add9e4b6b9dad69b30c0be194a92557
riordan.santos <riordan3607@gmail.com>
2023-01-04 16:28:06 -0300

    Creating useFetch

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -12,14 +12,17 @@ const Login = () => {
   if (login === true) return <Navigate to="/conta" />;
 
   return (
-    <div>
+    <section className={styles.login}>
+    <div className={styles.forms}>
       <Routes>
         <Route path="/" element={<LoginForm />} />
         <Route path="criar" element={<LoginCreate />} />
         <Route path="perdeu" element={<LoginPasswordLost />} />
         <Route path="resetar" element={<LoginPasswordReset />} />
+       
       </Routes>
     </div>
+  </section>
   );
 };
 export default Login;
