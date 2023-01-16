d4c024092d73ced09369c4488b032c9ad1aca860
riordan.santos <riordan3607@gmail.com>
2023-01-16 10:43:05 -0300

    feat(): UserStats

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -6,6 +6,7 @@ import UserHeader from './UserHeader';
 import UserPhotoPost from './UserPhotoPost';
 import Feed from '../Feed/Feed';
 import NotFound from '../NotFound';
+import UserStats from './UserStats';
 
 const User = () => {
   const { data } = React.useContext(UserContext);
@@ -17,8 +18,8 @@ const User = () => {
       <Routes>
         <Route path="/" element={<Feed user={data.id} />} />
         <Route path="postar" element={<UserPhotoPost />} />
+        <Route path="estatisticas" element={<UserStats />} />
         <Route path="*" element={<NotFound />} />
-  
       </Routes>
     </section>
   );
